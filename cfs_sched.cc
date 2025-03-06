61e9895daa4e15d14f4370e07b1030467c11ef05
AlekyaVeluri <aveluri@ucdavis.edu>
2025-03-05 23:42:41 -0800

    :(

--- cfs_sched.cc
+++ cfs_sched.cc
@@ -1,97 +1,93 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <memory>
 #include <string>
-#include <utility>
-#include <vector>
-#include <chrono>
 
 #include "map.h"
 
-
-
-
 struct Task {
-    std::string identifier;
-    int duration_time;
-    int remaining_time;
-    int vruntime;
+    char identifier;
+    unsigned int start_time;
+    unsigned int duration;
+    unsigned int vruntime;
 };
+
+class CFS_Scheduler {
+    public:
+    void Scheduler();
+
+    private:
+    unsigned int tick = 0;
+    unsigned int min_vruntime = 0;
+    Multimap<int, Task> timeline;
+    std::vector<Task> tasks;
+
+
+};
+
+void CFS_Scheduler::Scheduler(){
+    while(!tasks.empty()){
+        for(auto task : tasks){
+            if(task.start_time == tick){
+                task.vruntime = min_vruntime;
+                timeline.Insert(task.vruntime, task);
+                //somehow erase task from vector
+            }
+        }
+
+        if(timeline.Size() != 0){
+            auto current_task = timeline.Get(timeline.Min());
+            std::cout << tick <<"[" << timeline.Size() << "]:" << current_task.identifier;
+            current_task.duration--;
+
+            if(current_task.duration == 0){
+                std::cout << "*";
+                timeline.Remove(timeline.Min());
+            } else {
+                current_task.vruntime++;
+                timeline.Remove(timeline.Min());
+                timeline.Insert(current_task.vruntime, current_task);
+            }
+            min_vruntime = current_task.vruntime;
+        }
+        tick++;   
+    }
+
+}
 
 int main(int argc, char* argv[]){
     if(argc != 2){
-        std::cout << "Usage: ./cfs_sched <task_file.dat>";
+        std::cout << "Usage: ./cfs_sched <task_file.dat>" << std::endl;
+        return 1;
     }
+
     std::ifstream file(argv[1]);
 
     if(!file){
-        std::cout << "Error: cannot open file doesnotexist.dat";
+        std::cout << "Error: cannot open file " << argv[1] << std::endl;
+        return 1;
     }
-    Multimap<int, Task> task_map;
 
+    // Use multimap to store tasks by start time
+    std::vector<Task> tasks;
   
     std::string line;
     while(std::getline(file, line)){
         std::istringstream task(line);
-        std::string identifier;
+        char identifier;
         unsigned int start_time;
         unsigned int duration_time;
 
         task >> identifier >> start_time >> duration_time;
-        Task t = {identifier, duration_time, duration_time, 0};
-        task_map.Insert(start_time, t);
-    }
-    
 
-auto Timer (Multimap task_map) { 
-    unsigned int tick = 0;
-    unsigned int global_vruntime = 0;
-    std::vector<Task> timeline;
-    while(task_map.Size() != 0){
-        auto current_task = task_map.Get(task_map.Min()); // how to find other tasks with same key value??
-        timeline.push_back(current_task);
-        task_map.Remove(task_map.Min());
-        current_task.vruntime = global_vruntime;
-        tick++;
-        current_task.vruntime++;
-        if (current_task.vruntime > global_vruntime) {
-            Timer(task_map);
-        }
-
+        // Create task and add it to the map
+        Task t = {identifier, start_time, duration_time, 0}; // Set vruntime to 0 initially
+        tasks.push_back(t);
     }
+
+    CFS_Scheduler sched;
+    sched.Scheduler();
+
+
+    return 0;
 }
-
-   
-// need smth for timeline
-    unsigned int tick = 0;
-    unsigned int global_vruntime = 0;
-    std::vector<Task> timeline;
-    while(task_map.Size() != 0){
-        auto current_task = task_map.Get(task_map.Min()); // how to find other tasks with same key value??
-        timeline.push_back(current_task);
-        task_map.Remove(task_map.Min());
-        current_task.vruntime = global_vruntime;
-        tick++;
-        current_task.vruntime++;
-        if (current_task.vruntime > global_vruntime) {
-            timeline.push_back() // push next task onto the timeline
-
-        }
-
-
-
-
-
-
-
- }
-
-
-
-        
-        
-    }
-
-}
-
